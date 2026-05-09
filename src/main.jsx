fd49bccc0245a2dd44148b826e2ff0990d8c395a
Elijah Charo <elijahcharo285@gmail.com>
2026-05-09 01:40:47 -0500

    Add landing page generator route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import ElijahCharoPortfolio from './App'
+import LandingPageGenerator from './tools/LandingPageGenerator'
+
+function Root() {
+  const normalizedPath = window.location.pathname.replace(/\/$/, '') || '/'
+
+  if (normalizedPath === '/tools/landing-page-generator') {
+    return <LandingPageGenerator />
+  }
+
+  return <ElijahCharoPortfolio />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ElijahCharoPortfolio />
-  </React.StrictMode>
+    <Root />
+  </React.StrictMode>,
 )
